033913812fce635c6b7f90588ea58c8ec884e969
Mafraha01 <mafuoffcial@gmailcom>
2024-02-13 12:00:15 +0530

    Database connection file added

--- register.php
+++ register.php
@@ -1,17 +1,4 @@
 <?php
-// Database connection parameters
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "library_system";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Retrieve user input
